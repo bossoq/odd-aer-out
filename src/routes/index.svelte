baf1aa8a2840f767e19c0b71237b65fb9bbc058a
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-04 13:57:14 +0700

    Change Title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+  import Canvas from '$lib/components/Canvas.svelte'
 </script>
 
 <div class="w-full h-screen flex flex-col justify-center items-center bg-white dark:bg-black">
-  <h1 class="text-6xl dark:text-teal-200 text-teal-800 flex flex-col">
-    <span>Simple</span><span>Sveltekit</span><span>Template</span>
+  <h1 class="text-6xl text-black dark:text-white">
+    <span>Odd</span><span class="dark:text-teal-200 text-teal-800 mx-4">"แ"</span><span>Out</span>
   </h1>
+  <span class="my-4" />
+  <div class="flex flex-col justify-center items-center">
+    <Canvas />
+  </div>
 </div>
