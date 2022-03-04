c52550d780f170be5fb3dbb487ee652616680f66
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-04 21:09:32 +0700

    Adjust modal layout

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -14,9 +14,7 @@
     class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
   >
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75" aria-hidden="true">
-      <span class="hidden sm:inline-block sm:align-middle h-1/2 sm:h-screen" aria-hidden="true"
-        >&#8203;</span
-      >
+      <span class="inline-block align-middle h-1/6 sm:h-screen" aria-hidden="true">&#8203;</span>
       <div
         class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
         use:clickOutside
