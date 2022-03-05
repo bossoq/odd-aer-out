b8fd13b1e730a85c9f2b53806e3f956f7cbf54ae
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-05 20:21:30 +0700

    New Main Menu

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,8 @@
   const timerArr = [30, 45, 60]
 
   $: txtArr = randStringGenerator(16)
-  $: played = true // todo: change to false
-  $: timeup = false // todo: change to true
+  $: played = false
+  $: timeup = true
   $: copied = false
 
   const handleRandom = () => {
@@ -19,6 +19,10 @@
     played = true
     timeup = false
     handleRandom()
+  }
+  const handleTimerChange = () => {
+    $timer = timerArr.shift()
+    timerArr.push($timer)
   }
   const handleTimeUp = () => {
     timeup = true
@@ -103,32 +107,21 @@
     </div>
   {:else}
     <div
-      class="flex flex-col items-center justify-center text-lg sm:text-2xl text-black dark:text-white"
+      class="flex flex-col gap-2 items-center justify-center text-lg sm:text-2xl text-black dark:text-white"
     >
-      <div class="flex flex-row items-center justify-center gap-4">
-        <span>ตั้งค่าเวลา</span>
-        <div class="flex gap-2 justify-center">
-          {#each timerArr as t}
-            <div class="form-check form-check-inline">
-              <label class="form-check-label inline-block">
-                <input
-                  class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-400 dark:border-gray-200 bg-white checked:bg-teal-600 checked:border-teal-600 transition duration-200 mt-2 mr-1 cursor-pointer"
-                  type="radio"
-                  bind:group={$timer}
-                  name="timer"
-                  value={t}
-                />
-                {t} s</label
-              >
-            </div>
-          {/each}
-        </div>
-      </div>
-      <hr class="my-4 border-slate-300 dark:border-slate-600 w-full" />
       <button
-        class="text-white dark:text-black bg-teal-600 dark:bg-teal-300 rounded mt-1 p-3 font-medium"
-        on:click={handleStart}>เริ่มเกม</button
+        class="w-full text-teal-800 dark:text-teal-200 border-4 border-teal-600 dark:border-teal-300 rounded-2xl mt-1 p-4 font-medium uppercase"
+        on:click={handleStart}
       >
+        Start
+      </button>
+      <button
+        class="w-full text-teal-800 dark:text-teal-200 border-4 border-teal-600 dark:border-teal-300 rounded-2xl mt-1 p-4 font-medium uppercase"
+        on:click={handleTimerChange}
+      >
+        Timer: {$timer}s
+      </button>
+      <!-- <hr class="my-4 border-slate-300 dark:border-slate-600 w-full" /> -->
     </div>
   {/if}
 </div>
